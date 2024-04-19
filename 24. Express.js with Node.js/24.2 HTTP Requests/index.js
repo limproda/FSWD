37e1214778fa936c6e6fa4a60a0497e0fa38eaeb
Luigi Michele Improda <limproda@gmail.com>
2024-04-18 20:52:28 -0500

    Finished HTTP Request

--- 24. Express.js with Node.js/24.2 HTTP Requests/index.js
+++ 24. Express.js with Node.js/24.2 HTTP Requests/index.js
@@ -3,15 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+  res.send("<h1>Hello!</h1>");
 });
 
 app.get("/about", (req, res) => {
-  res.send("<h1>About Me</h1><p>My name is Angela</p>");
+  res.send("<h1>About Me</h1><p>My name is Luigi</p>");
 });
 
 app.get("/contact", (req, res) => {
-  res.send("<h1>Contact Me</h1><p>Phone: +44123456789</p>");
+  res.send("<h1>Contact Me</h1><p>Phone: +1 123456789</p>");
 });
 
 app.listen(port, () => {
